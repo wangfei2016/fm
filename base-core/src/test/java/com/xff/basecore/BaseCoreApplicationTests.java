f7b52c6b6f002e0d2e3192973a204af28a429cf1
wangfei2016 <1325561391@qq.com>
2022-04-01 18:41:38 +0800

    serviceSmgl

--- base-core/src/test/java/com/xff/basecore/BaseCoreApplicationTests.java
+++ base-core/src/test/java/com/xff/basecore/BaseCoreApplicationTests.java
@@ -1,10 +1,10 @@
-package com.xff.fm;
+package com.xff.basecore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FmApplicationTests {
+class BaseCoreApplicationTests {
 
     @Test
     void contextLoads() {
